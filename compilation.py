30cc7b23b4886d36fe2a1f3382f97da12537c829
Dominik Pieczyński <dominik.pieczynski@gmail.com>
2024-03-11 13:57:58 +0100

    Add browse template

---
+++ compilation.py
@@ -0,0 +1,96 @@
+import shutil
+import subprocess
+from pathlib import Path
+from typing import Any
+
+from git import Repo
+from jinja2 import Environment
+
+
+def compile_repo(display_name: str, name: str, repo: Repo, output_dir: Path, jinja_env: Environment,
+                 resources_path: Path) -> dict[str, Any]:
+    src_dir = Path(repo.working_dir) / 'src'
+    output_dir = output_dir
+
+    if output_dir.exists():
+        shutil.rmtree(output_dir)
+
+    output_dir.mkdir()
+
+    for entry in src_dir.iterdir():
+        if entry.name.startswith('_') or entry.name.startswith('.'):
+            if entry.is_dir():
+                shutil.copytree(entry, output_dir / entry.name)
+            else:
+                shutil.copyfile(entry, output_dir / entry.name)
+
+    entry = _compile_directory(src_dir, repo, output_dir, output_dir, jinja_env)
+    entry['name'] = display_name
+    entry['sorting_name'] = name
+    entry['url'] = output_dir.name
+
+    shutil.copytree(resources_path, output_dir, dirs_exist_ok=True)
+
+    return entry
+
+
+def _compile_directory(directory: Path, repo: Repo, base_output_dir: Path, output_dir: Path,
+                       jinja_env: Environment) -> dict[str, Any]:
+    entries = []
+    latest_datetime = None
+    for entry in directory.iterdir():
+        if entry.name.startswith('_') or entry.name.startswith('.'):
+            continue
+
+        if entry.is_dir():
+            entry = _compile_directory(entry, repo, base_output_dir, output_dir / entry.name, jinja_env)
+        elif entry.suffix == '.md':
+            entry = _compile_file(entry, repo, base_output_dir, output_dir)
+        else:
+            continue
+
+        entries.append(entry)
+        entry_datetime = entry['modification_time']
+        if latest_datetime is None or entry_datetime > latest_datetime:
+            latest_datetime = entry_datetime
+
+    template = jinja_env.get_template('browse.html')
+    rendered_html = template.render(
+        can_go_up=True,
+        entries=entries
+    )
+    with open(output_dir / 'index.html', 'w') as output_file:
+        output_file.write(rendered_html)
+
+    return {
+        'name': directory.name,
+        'url': str(output_dir.relative_to(base_output_dir)),
+        'modification_time': latest_datetime,
+        'is_dir': True
+    }
+
+
+def _compile_file(document_path: Path, repo: Repo, base_output_dir: Path, output_dir: Path) -> dict[str, Any]:
+    output_dir.mkdir(exist_ok=True, parents=True)
+
+    title = document_path.name.replace('.md', '')
+    output_file_name = f'{title}.html'
+    subprocess.run(['pandoc', str(document_path), '--from=gfm',
+                    '--to=html', '--standalone', '--mathjax',
+                    f'--metadata=title:{title}',
+                    f'--resource-path={base_output_dir}',
+                    f'--include-in-header=_pandoc_head.html',
+                    f'--include-before-body=_pandoc_header.html',
+                    f'--include-after-body=_pandoc_footer.html',
+                    '--css=_static/github-markdown-light.css',
+                    '--css=_static/custom.css',
+                    f'--output={output_dir / output_file_name}']
+                   )
+
+    commit = next(repo.iter_commits(paths=str(document_path)))
+    return {
+        'name': title,
+        'url': output_file_name,
+        'modification_time': commit.committed_datetime,
+        'is_dir': False
+    }
---
+++ run.py
@@ -0,0 +1,73 @@
+import json
+import os
+import shutil
+from pathlib import Path
+
+from git import Repo
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+from compilation import compile_repo
+
+
+def main():
+    access_token = os.environ.get('ACCESS_TOKEN')
+
+    with open('repositories_list.txt', 'r') as file:
+        repositories = file.read().split('\n')
+        repositories = [repo.strip() for repo in repositories if len(repo) > 0]
+
+    site_path = Path('_site')
+    resources_path = Path('resources')
+    repositories_path = Path('repositories')
+
+    site_path.mkdir(parents=True, exist_ok=True)
+    repositories_path.mkdir(parents=True, exist_ok=True)
+
+    # Copy static files for the site
+    shutil.copytree(resources_path / 'site', site_path, dirs_exist_ok=True)
+
+    jinja_env = Environment(
+        loader=FileSystemLoader('templates'),
+        autoescape=select_autoescape()
+    )
+
+    entries = []
+    for repo in repositories:
+        repo_name = repo.split('/')[-1].split('.')[0]
+
+        repo_dir = Path(f'repositories/{repo_name}')
+        if not repo_dir.exists():
+            print(f'Cloning {repo}...')
+
+            repo_request = f'https://dzbanobot:{access_token}@' + repo.split('https://')[1]
+            repo = Repo.clone_from(repo_request, repo_dir)
+        else:
+            repo = Repo(repo_dir)
+
+        config = load_json(repo_dir / 'config.json')
+        lang = config['lang']
+        name = config['name']
+        display_name = f'{lang} {name}'
+
+        print(f'Building {repo_name}...')
+        entries.append(
+            compile_repo(display_name, name, repo, site_path / repo_name, jinja_env, resources_path / 'pandoc')
+        )
+
+    # Build index for the root
+    template = jinja_env.get_template('browse.html')
+    rendered_html = template.render(
+        can_go_up=False,
+        entries=sorted(entries, key=lambda entry: entry['sorting_name'])
+    )
+    with open(site_path / 'index.html', 'w') as output_file:
+        output_file.write(rendered_html)
+
+
+def load_json(path: Path) -> dict | list:
+    with open(path, 'r') as file:
+        return json.load(file)
+
+
+if __name__ == '__main__':
+    main()
